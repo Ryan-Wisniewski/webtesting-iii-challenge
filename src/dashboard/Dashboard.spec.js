1ac8273bbbd184b4cdeaa393c827b9e5fcb61402
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-08-22 15:53:50 -0400

    i understand it

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,19 +1,44 @@
 // Test away
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import Dashbaord from './Dashboard'
 
 describe('<Dashboard />', () => {
     it('renders without crashing', () =>{
         render(<Dashbaord />)
     })
-    it('default state open and unlocked',() => {
-        const { getByText } = render(<Dashbaord />)
+})
+describe('<Dashboard /> state transition', () => {
+    
+    it('default state open and unlocked',() => {  
+        const { getByText } = render(<Dashbaord />)     
         //verify open and unlocked
         getByText(/^open$/i)
         getByText(/^unlocked$/i)
 
         const lockBtn = getByText(/^lock gate$/i)
+        const closeBtn = getByText(/^close gate$/i)        
+    })
+    it('open and unlocked to closed and unlocked', () => {
+        const { getByText } = render(<Dashbaord />)
         const closeBtn = getByText(/^close gate$/i)
+        fireEvent.click(closeBtn)
+        //check display text
+        getByText(/^closed$/i)
+        getByText(/^unlocked$/i)
+        //check button text
+        getByText(/^lock gate$/i)
+        getByText(/^open gate$/i)
+    })
+    it('closed and unlocked to closed and locked', () => {
+        const { getByText } = render(<Dashbaord />)
+        const lockBtn = getByText(/^lock gate$/i)
+        fireEvent.click(lockBtn)
+        //check display text
+        getByText(/^closed$/i)
+        getByText(/^locked$/i)
+        //check button text
+        getByText(/^unlock gate$/i)
+        getByText(/^open gate$/i)
     })
 })
