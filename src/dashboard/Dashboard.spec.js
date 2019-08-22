0acd7a0604011e58a014ba933caeb089f6c436b5
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-08-22 10:59:34 -0400

    almost done

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
 // Test away
+import React from 'react'
+import {render} from '@testing-library/react'
+import Dashbaord from './Dashboard'
+
+describe('<Dashboard />', () => {
+    it('renders without crashing', () =>{
+        render(<Dashbaord />)
+    })
+    it('default state open and unlocked',() => {
+        const { getByText } = render(<Dashbaord />)
+        //verify open and unlocked
+        getByText(/^open$/i)
+        getByText(/^unlocked$/i)
+
+        const lockBtn = getByText(/^lock gate$/i)
+        const closeBtn = getByText(/^close gate$/i)
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,32 @@
 // Test away!
+import React from 'react'
+import Display from './Display'
+import {render} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+// afterEach(cleanup)
+
+describe('<Dislplay />', ()=> {
+    it('renders without crashing', () => {
+        render(<Display />)
+    })
+    it('open and unlocked', () => {
+        const { getByText, queryByText } = render(<Display closed={false} locked={false}/>)
+        const unlock = getByText(/unlocked/i)
+        const open = getByText(/open/i)
+        expect(unlock.className).toMatch(/green-led/i)
+        expect(open.className).toMatch(/green-led/i)
+        //checks to see state is not closed does not exsist on first render
+        expect(queryByText(/closed/i)).toBeFalsy()
+    })
+    it('closed and unlocked', () => {
+        const { getByText, queryByText } = render(<Display closed={true} locked={false}/>)
+        const unlock = getByText(/unlocked/i)
+        const clocsed = getByText(/closed/i)
+    })
+    it('closed and locked', () => {
+        const { getByText, queryByText } = render(<Display closed={true} locked={true}/>)
+        const locked = getByText(/^locked$/i)
+        const closed = getByText(/closed/i)
+    })
+})
